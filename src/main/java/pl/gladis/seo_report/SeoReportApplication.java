a27a77b414a8f1f9b4bf4e7040752ad52a6409d4
Martin Gladis <martin@pc-5884.home>
2020-10-13 12:27:44 +0200

    First Add. Create Project

---
+++ src/main/java/pl/gladis/seo_report/SeoReportApplication.java
@@ -0,0 +1,13 @@
+package pl.gladis.seo_report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeoReportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeoReportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/gladis/seo_report/SeoReportApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.gladis.seo_report;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeoReportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
